ebccaa5c4e4789400b905a3a281f2eae09f1ddee
DiabloBlood <huangsichao2015@gmail.com>
2019-02-15 14:10:32 -0800

    Added: Counting Sort of 49

--- line/hash/hash_49.java
+++ line/hash/hash_49.java
@@ -14,10 +14,47 @@ Output:
 
 public class Solution49 {
 
+    /*
+    * Assume `m` is the average length of s, `n` is the length of string array `strs`
+    * Time Complexity: O((128 + m)*n), since the counting sort is O(m)
+    * Space Complexity: O(m), for StringBuilder usage
+    */
+    public List<List<String>> groupAnagrams(String[] strs) {
+        if (strs == null) {
+            return new ArrayList<>();
+        }
+        Map<String, List<String>> map = new HashMap<>();
+        for (String s: strs) {
+            String key = countingSort(s);
+            if (!map.containsKey(key)) {
+                map.put(key, new ArrayList<>());
+            }
+            map.get(key).add(s);
+        }
+        return new ArrayList<>(map.values());
+    }
+
+    private String countingSort(String s) {
+        if (s == null || s == "") {
+            return s;
+        }
+        int[] map = new int[128];
+        for (int i = 0; i < s.length(); i++) {
+            map[s.charAt(i)]++;
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < map.length; i++) {
+            for (int j = 0; j < map[i]; j++) {
+                sb.append(i);
+            }
+        }
+        return sb.toString();
+    }
 
     /*
     * Assume `m` is the average length of s, `n` is the length of string array `strs`
-    * Time Complexity: 
+    * Time Complexity: O(m*logm*n), since the sort is m*logm
+    * Space Complexity: O(m), for char array usage.
     */
     public List<List<String>> groupAnagrams2(String[] strs) {
         if (strs == null) {
