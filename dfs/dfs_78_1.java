5a9686683ed2cc4dd018b8bd4a8094d4e8384fd8
DiabloBlood <huangsichao2015@gmail.com>
2019-02-17 23:46:32 -0800

    Added: Mathematical Induction method of 78 subsets

--- dfs/dfs_78_1.java
+++ dfs/dfs_78_1.java
@@ -1,34 +1,28 @@
-
-
-
 /*
-Given a set of distinct integers, nums,
-return all possible subsets (the power set).
+Given a collection of integers that might contain duplicates, nums, return all possible subsets (the power set).
 
 Note: The solution set must not contain duplicate subsets.
 
 Example:
 
-Input: nums = [1,2,3]
+Input: [1,2,2]
 Output:
 [
-  [3],
-  [1],
   [2],
-  [1,2,3],
-  [1,3],
-  [2,3],
+  [1],
+  [1,2,2],
+  [2,2],
   [1,2],
   []
 ]
 */
-
 import java.util.ArrayList;
 import java.util.List;
 
-public class Solution78 {
 
-    public List<List<Integer>> subsets(int[] nums) {
+public class Solution90 {
+
+        public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
         if (nums == null) {
             return result;
@@ -45,21 +39,27 @@ public class Solution78 {
         return result;
     }
 
-    public List<List<Integer>> subsets2(int[] nums) {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
         if (nums == null) {
             return result;
         }
+        Arrays.sort(nums);
         helper(nums, new ArrayList<>(), result, 0);
         return result;
     }
 
+    // dfs method
+    // Time complexity: O(n*2^n-1) is worst (no duplicate), O(n^2) is best (all input numbers is same)
+    // Space: O(n) Stack is always O(n), no matter all is same or duplicate. List is same, too.
     private void helper(int[] nums, List<Integer> list, List<List<Integer>> result, int index) {
         result.add(new ArrayList<>(list));
         for (int i = index; i < nums.length; i++) {
+            if (i > index && nums[i] == nums[i - 1]) {
+                continue;
+            }
             list.add(nums[i]);
             helper(nums, list, result, i + 1);
-            // remove last element
             list.remove(list.size() - 1);
         }
     }
