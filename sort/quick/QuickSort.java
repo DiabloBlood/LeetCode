8700327669f82be3908085cdf0668b7a8f87ee47
Diabloblood <huangsichao2015@gmail.com>
2019-07-28 16:55:05 -0700

    Added: Quick sort problems

--- sort/quick/QuickSort.java
+++ sort/quick/QuickSort.java
@@ -1,52 +1,98 @@
+import java.util.Arrays;
 
 
-
+/**
+ * Partition sub-routine:
+ *
+ * Input: [7, 3, 1, 3, 6, 2, 3]
+ * 
+ * pivot = 3
+ *  
+ *  i
+ *  f                 b
+ * [7, 3, 1, 3, 6, 2, 3]
+ *           |
+ *  i
+ *  f              b   
+ * [3, 3, 1, 3, 6, 2, 7]
+ *           |
+ *  f  i           b   
+ * [3, 3, 1, 3, 6, 2, 7]
+ *           |
+ *  f     i        b   
+ * [3, 3, 1, 3, 6, 2, 7]
+ *           |
+ *     f     i     b   
+ * [1, 3, 3, 3, 6, 2, 7]
+ *           |
+ *     f        i  b  
+ * [1, 3, 3, 3, 6, 2, 7]
+ *           |
+ *              i
+ *     f        b
+ * [1, 3, 3, 3, 2, 6, 7]
+ *           |
+ *        f     b  i
+ * [1, 2, 3, 3, 3, 6, 7]
+ *
+ * END
+ */
 public class QuickSort {
+    /**
+     * Case Analysis:
+     * 1. `0` ~ `low - 1` is the part which `< pivot`.
+     * 2. `low` ~ `high` is the part which `==pivot`.
+     * 3. `high + 1` ~ `n - 1` is the part which `> pivot`.
+     * 
+     * Time:  Best  O(nlogn)
+     *        Worst O(n^2 / 2) if the array is already sorted. The recursive tree is a flat list tree.
+     * Space: Best  O(logn)
+     *        Worst O(n)
+     */
+    public static void sort(int[] array) {
+        if (array == null) {
+            throw new NullPointerException();
+        }
+        quicksort(array, 0, array.length - 1);
+    }
 
-    public int[] partition(int[] array, int start, int end) {
-        int front = start - 1;
-        int back = end + 1;
-        int pivot = array[end];
-        int i = start;
-        while(i < back) {
+    private static void quicksort(int[] array, int low, int high) {
+        if (low >= high) {
+            return;
+        }
+        int[] bound = partition(array, low, high);
+        quicksort(array, low, bound[0] - 1);
+        quicksort(array, bound[0] + 1, high);
+    }
+
+    private static int[] partition(int[] array, int low, int high) {
+        int pivot = array[high];
+        int i = low;
+        while(i <= high) {
             if(array[i] < pivot) {
-                front++;
-                swap(array, front, i);
-                i++;
+                swap(array, low++, i++);
             } else if (array[i] == pivot) {
                 i++;
             } else {
-                back--;
-                swap(array, back, i);
+                swap(array, high--, i);
             }
         }
-        return new int[]{front, back};
+        return new int[]{low, high};
     }
 
 
-    private void swap(int[] nums, int p, int q) {
-        int temp = nums[p];
-        nums[p] = nums[q];
-        nums[q] = temp;
-    }
-
-    private void printArray(int[] array) {
-        for(int i = 0; i < array.length; i++) {
-            System.out.print(array[i]);
-            if(i < array.length - 1) {
-                System.out.print(", ");
-            }
-        }
-        System.out.println();
+    private static void swap(int[] array, int p, int q) {
+        int temp = array[p];
+        array[p] = array[q];
+        array[q] = temp;
     }
 
     public static void main(String[] args) {
-        QuickSort ss = new QuickSort();
-        //int[] array = {85, 6, 57, 60, 83, 73, 72};
-        int[] array = {5, 5, 5, 5, 5};
-        //ss.printArray(array);
-        int[] result = ss.partition(array, 0, array.length - 1);
-        //ss.printArray(result);
-        //ss.printArray(array);
+        int[] array = {85, 6, 57, 60, 83, 73, 72};
+        QuickSort.sort(array);
+        System.out.println(Arrays.toString(array));
+        int[] array2 = {7, 3, 1, 3, 6, 2, 3};
+        QuickSort.partition(array2, 0, array2.length - 1);
+        System.out.println(Arrays.toString(array2));
     }
 }
--- sort/quick/sort_75.java
+++ sort/quick/sort_75.java
@@ -1,28 +1,32 @@
 
 
-// also could use counting sort
 
-// one-pass O(n)
-public class Solution {
+public class Solution75 {
 
+    /**
+     * Notes: Also could use two-pass counting sort 
+     *
+     * Times: O(n), one-pass
+     * Space: O(1)
+     */
     public void sortColors(int[] nums) {
-        int leftBound = -1;
-        int rightBound = nums.length;
-        int i = 0;
-        while(i < rightBound) {
-            if(nums[i] == 0) {
-                leftBound++;
-                swap(nums, leftBound, i);
-                i++;
-            } else if(nums[i] == 1) {
+        if (nums == null) {
+            return;
+        }
+        int low = 0;
+        int high = nums.length - 1;
+        int i = low;
+        while (i <= high) {
+            if (nums[i] == 0) {
+                swap(nums, low++, i++);
+            } else if (nums[i] == 1) {
                 i++;
             } else {
-                rightBound--;
-                swap(nums, rightBound, i);
+                swap(nums, high--, i);
             }
         }
     }
-
+    
     private void swap(int[] nums, int p, int q) {
         int temp = nums[p];
         nums[p] = nums[q];
