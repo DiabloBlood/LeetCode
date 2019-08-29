c748ecaa1af51884e5814754aa67c042a8f3cf59
Diabloblood <huangsichao2015@gmail.com>
2019-08-29 16:59:09 -0700

    Added: DP 91 follow up

--- dp/dp_91.java
+++ dp/dp_91.java
@@ -16,12 +16,12 @@ class Solution91 {
         if (s == null || s.length() == 0) {
             return 0;
         }
-        int prev = 1;                           // case i == -1
-        int next = s.charAt(0) == '0' ? 0 : 1;  // case i == 0
+        int prev = 1;                           // node i == -1
+        int next = s.charAt(0) == '0' ? 0 : 1;  // node i == 0
         for (int i = 1; i < s.length(); i++) {
             int cur = 0;
-            int temp = Integer.parseInt(s.substring(i - 1, i + 1));
-            if (temp >= 10 && temp <= 26) {
+            int value = Integer.parseInt(s.substring(i - 1, i + 1));
+            if (value >= 10 && value <= 26) {
                 cur += prev;
             }
             if (s.charAt(i) != '0') {
@@ -31,6 +31,53 @@ class Solution91 {
             next = cur;
         }
         return next;
+    }
+    /**
+     * A follow up to return all possible strings
+     *
+     * Test: input "1821", return [RU, AHU, RBA, AHBA]
+     */
+    public List<String> numDecodings(String s) {
+        if (s == null || s.length() == 0) {
+            return new ArrayList<>();
+        }
+        List<StringBuilder> prev = new ArrayList<>();
+        List<StringBuilder> next = new ArrayList<>();
+        prev.add(new StringBuilder());
+
+        if (s.charAt(0) != '0') {
+            int value = s.charAt(0) - '0';
+            char c = (char)(value - 1 + 'A');
+            next.add(new StringBuilder(c + ""));
+        }
+
+        for (int i = 1; i < s.length(); i++) {
+            List<StringBuilder> cur = new  ArrayList<>();
+            int value = Integer.parseInt(s.substring(i - 1, i + 1));
+            if (value >= 10 && value <= 26) {
+                char c = (char)(value - 1 + 'A');
+                for (StringBuilder sb: prev) {
+                    sb.append(c);
+                    cur.add(sb);
+                }
+            }
+            if (s.charAt(i) != '0') {
+                value = s.charAt(i) - '0';
+                char c = (char)(value - 1 + 'A');
+                for (StringBuilder sb: next) {
+                    StringBuilder copy = new StringBuilder(sb);
+                    copy.append(c);
+                    cur.add(copy);
+                }
+            }
+            prev = next;
+            next = cur;
+        }
+        List<String> result = new ArrayList<>();
+        for (StringBuilder sb: next) {
+            result.add(sb.toString());
+        }
+        return result;
     }
     /**
      * DP method with visited map.
@@ -52,8 +99,8 @@ class Solution91 {
         visited[1] = s.charAt(0) == '0' ? 0 : 1;
         
         for (int i = 1; i < n; i++) {
-            int temp = Integer.parseInt(s.substring(i - 1, i + 1));
-            if (temp >= 10 && temp <= 26) {
+            int value = Integer.parseInt(s.substring(i - 1, i + 1));
+            if (value >= 10 && value <= 26) {
                 visited[i + 1] += visited[i - 1];
             }
             if (s.charAt(i) != '0') {
@@ -90,8 +137,8 @@ class Solution91 {
             result = s.charAt(i) == '0' ? 0 : 1;
         }
         if (i > 0) {
-            int temp = Integer.parseInt(s.substring(i - 1, i + 1));
-            if (temp >= 10 && temp <= 26) {
+            int value = Integer.parseInt(s.substring(i - 1, i + 1));
+            if (value >= 10 && value <= 26) {
                 result += visited[i - 1] == -1 ? helper(s, visited, i - 2) : visited[i - 1];
             }
             if (s.charAt(i) != '0') {
@@ -128,8 +175,8 @@ class Solution91 {
         }
         // all cases of i > 0
         int result = 0;
-        int temp = Integer.parseInt(s.substring(i - 1, i + 1));
-        if (temp >= 10 && temp <= 26) {
+        int value = Integer.parseInt(s.substring(i - 1, i + 1));
+        if (value >= 10 && value <= 26) {
             result += helper(s, i - 2);
         }
         if (s.charAt(i) != '0') {
