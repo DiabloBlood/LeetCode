c0f14d6a24318e20e1e85118e536662b2881945f
DiabloBlood <huangsichao2015@gmail.com>
2019-10-15 07:17:42 +0000

    Added: detailed comments for 26

--- line/one_pass/line_26.java
+++ line/one_pass/line_26.java
@@ -1,37 +1,23 @@
 
 
 
-/*
-Given a sorted array nums, remove the duplicates in-place such that each element appear only once and return the new length.
-
-Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
-
-Example 1:
-
-Given nums = [1,1,2],
-
-Your function should return length = 2, with the first two elements of nums being 1 and 2 respectively.
-
-It doesn't matter what you leave beyond the returned length.
-Example 2:
-
-Given nums = [0,0,1,1,1,2,2,3,3,4],
-
-Your function should return length = 5, with the first five elements of nums being modified to 0, 1, 2, 3, and 4 respectively.
-
-It doesn't matter what values are set beyond the returned length.
-*/
-
-/*
-* 1. 0 -> end - 1 is valid
-* 2. end - 1 is the largest number in array
-* 3. if nums[i] > nums[end - 1], which means found next largest number
-* 4. end should be return length
-*/
-
 public class Solution26 {
-
-    // O(n)
+    /**
+     * Key Points:
+     *     1. This a sorted array, if this array is not sorted, either use hashmap or sort this array first.
+     *
+     * Problem Analysis:
+     *     1. [0, end) is valid
+     *     2. if `a[i] > a[end - 1]`, which means next larger number found.
+     *     3. `end` should be return length.
+     *
+     * Case Analysis:
+     *     1. a[i] >  a[end - 1]; ---> a[end++] = a[i]
+     *     2. a[i] == a[end - 1]; ---> // do nothing
+     *
+     * Time:  O(n)
+     * Space: O(1)
+     */
     public int removeDuplicates(int[] nums) {
         if (nums == null || nums.length == 0) {
             return 0;
@@ -45,7 +31,3 @@ public class Solution26 {
         return end;
     }
 }
-
-
-
-
