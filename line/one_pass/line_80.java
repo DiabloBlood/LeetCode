fb816e42e4907443e582105cbf7b18ce85969955
DiabloBlood <huangsichao2015@gmail.com>
2019-10-30 07:42:06 +0000

    Added: A more concise method for 80

--- line/one_pass/line_80.java
+++ line/one_pass/line_80.java
@@ -13,17 +13,17 @@ class Solution80 {
      *
      * Case Analysis:
      *     1. nums[end - 2] == nums[end - 1] && nums[end - 1] == nums[i]; ---> // do nothing;
-     *     2. nums[end - 2] == nums[end - 1] && nums[end - 1] <  nums[i]; ---> // nums[end++] = nums[i];
-     *     3. nums[end - 2] <  nums[end - 1] && nums[end - 1] == nums[i]; ---> // nums[end++] = nums[i];
-     *     4. nums[end - 2] <  nums[end - 1] && nums[end - 1] <  nums[i]; ---> // nums[end++] = nums[i];
+     *     2. nums[end - 2] == nums[end - 1] && nums[end - 1] <  nums[i]; ---> nums[end++] = nums[i];
+     *     3. nums[end - 2] <  nums[end - 1] && nums[end - 1] == nums[i]; ---> nums[end++] = nums[i];
+     *     4. nums[end - 2] <  nums[end - 1] && nums[end - 1] <  nums[i]; ---> nums[end++] = nums[i];
      *     =>
      *     1. nums[end - 2] == nums[i]; ---> // do nothing;
-     *     2. nums[end - 2] <  nums[i]; ---> // nums[end++] = nums[i];
-     *     3. nums[end - 2] <  nums[i]; ---> // nums[end++] = nums[i];
-     *     4. nums[end - 2] <  nums[i]; ---> // nums[end++] = nums[i];
+     *     2. nums[end - 2] <  nums[i]; ---> nums[end++] = nums[i];
+     *     3. nums[end - 2] <  nums[i]; ---> nums[end++] = nums[i];
+     *     4. nums[end - 2] <  nums[i]; ---> nums[end++] = nums[i];
      *     =>
      *     1. nums[end - 2] == nums[i]; ---> // do nothing;
-     *     2. nums[end - 2] <  nums[i]; ---> // nums[end++] = nums[i];
+     *     2. nums[end - 2] <  nums[i]; ---> nums[end++] = nums[i];
      *
      * Time:  O(n)
      * Space: O(1)
@@ -39,6 +39,26 @@ class Solution80 {
         int end = 2;
         for (int i = 2; i < len; i++) {
             if (nums[i] < nums[end - 2]) {
+                nums[end++] = nums[i];
+            }
+        }
+        return end;
+    }
+    /**
+     * A more concise method.
+     *
+     * Case Analysis:
+     *     1. end < 2;                  ---> nums[end++] = nums[i]; 
+     *     2. nums[end - 2] == nums[i]; ---> // do nothing;
+     *     3. nums[end - 2] <  nums[i]; ---> nums[end++] = nums[i]; 
+     */
+    public int removeDuplicates(int[] nums) {
+        if (nums == null) {
+            return -1;
+        }
+        int end = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (end < 2 || nums[end - 2] < nums[i]) {
                 nums[end++] = nums[i];
             }
         }
