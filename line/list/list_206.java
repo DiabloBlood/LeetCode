750f312610726b08c46cbd5bf485892b76871aaa
DiabloBlood <huangsichao2015@gmail.com>
2019-11-04 21:28:50 -0800

    Added: Tail recursion method for 206

--- line/list/list_206.java
+++ line/list/list_206.java
@@ -3,34 +3,52 @@
 
 class Solution206 {
     /*
-     * Iterative method
+     * Iterative method.
      * Time:  O(n)
      * Space: O(1)
      */
     public ListNode reverseList(ListNode head) {
         ListNode prev = null;
         while (head != null) {
-            ListNode nextHead = head.next;
+            ListNode next = head.next;
             head.next = prev;
             prev = head;
-            head =  nextHead;
+            head =  next;
         }
         return prev;
     }
 
     /*
-     * Recursive method
-     * Time:  O(2n)
-     * Space: O(n)
+     * Recursive method, not tail recursion.
+     * Time:  O(n), `n` nodes called, every node is O(1) times operation.
+     * Space: O(n), call stack depth is O(n).
      */
     public ListNode reverseList(ListNode head) {
         if (head == null || head.next == null) {
             return head;
         }
-        ListNode next = head.next;
+        ListNode newHead = reverseList(head.next);
+        head.next.next = head;
+        // for list 1 -> 2 -> 3 -> 4 -> null, if not assign head.next = null, finaly 1 <-> 2 will have cycle, but other nodes not.
         head.next = null;
-        ListNode newHead = reverseList(next);
-        next.next = head;
         return newHead;
+    }
+
+    /*
+     * Tail recusion method.
+     * Time:  O(n), `n` nodes called, every node is O(1) times operation.
+     * Space: O(1) or O(n). If compiler optimized tail recursion, space should be O(1).If not, space is O(n), call stack depth is O(n).
+     */
+    public ListNode reverseList(ListNode head) {
+        return helper(head, null);
+    }
+
+    private ListNode helper(ListNode head, ListNode prev) {
+        if (head == null) {
+            return prev;
+        }
+        ListNode next = head.next;
+        head.next = prev;
+        return helper(next, head);
     }
 }
