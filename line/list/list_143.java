7e96172477f502c3fe255000e0bd4dbbc56993c7
DiabloBlood <huangsichao2015@gmail.com>
2019-11-06 21:31:04 -0800

    Added detailed comments for 143

--- line/list/list_143.java
+++ line/list/list_143.java
@@ -12,17 +12,31 @@ class Solution143 {
      * Time:  O(n), findMid O(n/2), reverse (n/2), merge(n /2)
      * Space: O(1)
      */
-    public void reorderList(ListNode head) {
+public void reorderList(ListNode head) {
         if (head == null) {
             return;
         }
-        ListNode mid = getMid(head);
+        ListNode mid = findMid(head);
         ListNode l2 = reverse(mid.next);
         mid.next = null;
         merge(head, l2);
-        
     }
-    
+
+    /**
+     * Samples of merge method:
+     *     1. Input 1->2->3->4->null, merge l1: 1->2->null and l2: 4->3->null
+     *         - l1: 1->4->2->null, l2: 3->null
+     *         - l1: 1->4->2->3->null, l2: null
+     *         - Finally, l1 at `null`, l2 at `null`
+     *     2. Input 1->2->3->4->5->null, merge l1: 1->2->3->null and l2: 5->4->null
+     *         - l1: 1->5->2->3->null, l2: 4->null
+     *         - l1: 1->5->2->4->3->null, l2: null
+     *         - Finally, l1 at `3`, l2 at `null`
+     *
+     * Case Analysis of merge method:
+     *     1. l2 != null, merge
+     *     2. l2 == null, merge finished, stop while loop.
+     */
     private void merge(ListNode l1, ListNode l2) {
         while (l2 != null) {
             ListNode temp = l2.next;
@@ -32,8 +46,14 @@ class Solution143 {
             l2 = temp;
         }
     }
-
-    private ListNode getMid(ListNode head) {
+    
+    /**
+     * Key Points of findMid method:
+     *     1. `head` must not be `null`, otherwise, `fast.next` will throw `NullPointerException`.
+     *     2. For 1->2->3->4->null, finally `slow` at `2`.
+     *     3/ For 1->2->3->4->5->null, finally `slow` at `3`.
+     */
+    private ListNode findMid(ListNode head) {
         ListNode slow = head;
         ListNode fast = head;
         while (fast.next != null && fast.next.next != null) {
@@ -46,19 +66,11 @@ class Solution143 {
     private ListNode reverse(ListNode head) {
         ListNode prev = null;
         while (head != null) {
-            ListNode next = head.next;
-            head.next = prev;
-            prev = head;
-            head = next;
+          ListNode next = head.next;
+          head.next = prev;
+          prev = head;
+          head = next;
         }
         return prev;
     }
 }
-
-
-
-
-
-
-
-
