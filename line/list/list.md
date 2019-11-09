98cd30e7623040d69885c143029b6e04a590c34b
DiabloBlood <huangsichao2015@gmail.com>
2019-11-08 21:21:52 -0800

    Added: some comments of 148

---
+++ line/list/list.md
@@ -0,0 +1,8 @@
+### Linked List Notes
+------
+1. Reverse
+    - 206, 143, 234
+2. Slow & Fast Pointer
+    - 143, 234, 148
+3. Merge
+    - 21, 143, 148 (Sort)
--- line/list/list_148.java
+++ line/list/list_148.java
@@ -7,11 +7,14 @@ class Solution148 {
      * Space: O(1)
      */
     public ListNode sortList(ListNode head) {
+        if (head == null) {
+            return null;
+        }
         return mergeSort(head);
     }
-    
+
     private ListNode mergeSort(ListNode head) {
-        if (head == null || head.next == null) {
+        if (head.next == null) {
             return head;
         }
         ListNode mid = findMid(head);
@@ -24,16 +27,16 @@ class Solution148 {
     private ListNode findMid(ListNode head) {
         ListNode slow = head;
         ListNode fast = head;
-        while(fast.next != null && fast.next.next != null) {
+        while (fast.next != null && fast.next.next != null) {
             slow = slow.next;
             fast = fast.next.next;
         }
         return slow;
     }
-    
+
     private ListNode merge(ListNode l1, ListNode l2) {
-        ListNode fakeHead = new ListNode(-1);
-        ListNode cur = fakeHead;
+        ListNode dummy = new ListNode(-1);
+        ListNode cur = dummy;
         while (l1 != null && l2 != null) {
             if (l1.val < l2.val) {
                 cur.next = l1;
@@ -45,17 +48,30 @@ class Solution148 {
             cur = cur.next;
         }
         cur.next = l1 == null ? l2 : l1;
-        return fakeHead.next;
+        return dummy.next;
     }
 }
 
 
-
-
-
-
-
-
-
-
-
+/*
+    // pending quicksort method
+    private ListNode partition(ListNode low, ListNode high) {
+        int pivot = high.val;
+        ListNode dummy = ListNode(-1);
+        dummy.next = low;
+        ListNode i = dummy;
+        ListNode j = dummy;
+        while (j.next != high) {
+            if (j.next.val < pivot) {
+                ListNode temp = i.next;
+                ListNode temp2 = j.next.next;
+                j.next.next = i.next.next;
+                i.next = j.next;
+                j.next = temp;
+                j.next.next = temp2;
+                i = i.next;
+            }
+            j = j.next;
+        }
+    }
+*/
