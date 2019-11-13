dd94843f404626e8fa73f8841058539de61ac58d
DiabloBlood <huangsichao2015@gmail.com>
2019-11-12 19:07:48 -0800

    Modified: comments of 86, 328

--- line/list/list_328.java
+++ line/list/list_328.java
@@ -2,11 +2,30 @@
 
 
 class Solution328 {
-    /* 
+    /**
+     * Problem Analysis:
+     *     1. List length is odd number, `even` finally at `null`.
+     *        Input is `1->2->3->4->5->null`, output should be `1->3->5->2->4->null`.
+     *        init state: odd list: `1->2->3->4->5->null`, ptr `odd` at `1`, even list: `2->3->4->5->null`, ptr `even` at `2`.
+     *        loop 1 end: odd list: `1->3->4->5->null`, ptr `odd` at `3`, even list: `2->4->5->null`, ptr `even` at `4`.
+     *        loop 2 end: odd list: `1->3->5->null`, ptr `odd` at `5`, even list: `2->4->null`, ptr `even` at `null`.
+     *        loop 3 end: loop 3 will break, finally assign `odd.next = evenHead`, then return `head`.
+     *     2. List length is even number, `even` finally at `tail`.
+     *        Input is `1->2->3->4->5->6->null`, output should be `1->3->5->2->4->6->null`.
+     *        init state: odd list: `1->2->3->4->5->6->null`, ptr `odd` at `1`, even list: `2->3->4->5->6->null`, ptr `even` at `2`.
+     *        loop 1 end: odd list: `1->3->4->5->6->null`, ptr `odd` at `3`,  even list: `2->4->5->6->null`, ptr `even` at `4`.
+     *        loop 2 end: odd list: `1->3->5->6->null`, ptr `odd` at `5`,  even list: `2->4->6->null`, ptr `even` at `6`.
+     *        loop 3 end: loop 3 will break, finally assign `odd.next = evenHead`, then return `head`.
+     *
      * Case Analysis:
-     * 1. n is odd, even finally at null
-     * 2. n is even, even finally at the tail
-     * Time:  O(n)
+     *     1. even == null || even.next == null; ---> while loop break.
+     *     2. even != null && even.next != null; ---> while loop continue. (Use demorgan's law)
+     *
+     * Corner cases:
+     *     1. head == null;      ---> should be handled, otherwise `ListNode even = head.next` will throw `NullPointerException`.
+     *     2. head.next == null; ---> doesn't need to handle.
+     *
+     * Time:  O(n), exactly one pass.
      * Space: O(1)
      */
     public ListNode oddEvenList(ListNode head) {
--- line/list/list_86.java
+++ line/list/list_86.java
@@ -3,29 +3,43 @@
 
 class Solution86 {
     /**
-     * Case Analysis:
-     *      After loop end, one of left.next and right.next is not null. It depends on if tail.val < x or >= x.
-     * Time:  O(n)
+     * Partition function is a subrotine of quicksort.
+     *
+     * Problem Pitfalls:
+     *     1. After loop end, one of l1.next and l2.next is not null. It depends on if tail.val < x or >= x.
+     *        Finally assign `l2.next = null`, please refer to Problem Analysis step (3).
+     *
+     * Problem Analysis:
+     *     1. Consider input `1->4->3->2->5->2->null`, `x = 3`, result should be `1->2->2->4->3->5`.
+     *     2. Use 2 dummy nodes, left dummy node hold list with nodes less than `x`,
+     *        right dummy node hold list with nodes greater or equal than `x`.
+     *     3. After partition, list dummy1 is `dummy1->1->2->2->null`, list dummy2 is `dummy2->4->3->5->2->null`,
+     *        ptr l1 at `2`, ptr l2 at `5`, we should assign `l2.next = null`.
+     *     4. Finally connect two lists, assign `l1.next = dummy2.next`, then return `dummy1.next`.
+     *
+     * Corner Cases:
+     *     1. head == null; ---> doesn't need to handle, the final return value of `dummy1.next` is `null`.
+     *
+     * Time:  O(n), exactly one pass.
      * Space: O(1)
      */
     public ListNode partition(ListNode head, int x) {
-        ListNode leftDummy = new ListNode(-1);
-        ListNode rightDummy = new ListNode(-1);
-        ListNode left = leftDummy;
-        ListNode right = rightDummy;
-
+        ListNode dummy1 = new ListNode(-1);
+        ListNode dummy2 = new ListNode(-1);
+        ListNode l1 = dummy1;
+        ListNode l2 = dummy2;
         while (head != null) {
             if (head.val < x) {
-                left.next = head;
-                left = left.next;
+                l1.next = head;
+                l1 = l1.next;
             } else {
-                right.next = head;
-                right = right.next;
+                l2.next = head;
+                l2 = l2.next;
             }
             head = head.next;
         }
-        right.next = null;
-        left.next = rightDummy.next;
-        return leftDummy.next;
+        l2.next = null;
+        l1.next = dummy2.next;
+        return dummy1.next;
     }
 }
