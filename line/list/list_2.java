c87357a042bfe830d24bdd2fed819f46431f7d08
DiabloBlood <huangsichao2015@gmail.com>
2019-11-14 22:13:23 -0800

    Added: some comments of list 2

--- line/list/list_2.java
+++ line/list/list_2.java
@@ -3,77 +3,44 @@
 
 class Solution2 {
     /**
-     * Time: O(max(m, n))
+     * Problem Analysis:
+     *     1. Input: (2->4->3) + (5->6->4), Output: 7->0->8, Explanation: 342 + 465 = 807. l1 and l2 is reversed.
+     *     2. l1 and l2 do not contain any leading zero, except the number 0 itself.
+     *
+     * Case Analysis:
+     *     1. l1 != null && l2 != null && carry >  0; ---> continue
+     *     2. l1 != null && l2 != null && carry == 0; ---> continue
+     *     3. l1 != null && l2 == null && carry >  0; ---> continue
+     *     4. l1 != null && l2 == null && carry == 0; ---> continue
+     *     5. l1 == null && l2 != null && carry >  0; ---> continue
+     *     6. l1 == null && l2 != null && carry == 0; ---> continue
+     *     7. l1 == null && l2 == null && carry >  0; ---> continue
+     *     8. l1 == null && l2 == null && carry == 0; ---> break
+     *     combine these 8 cases:
+     *     1. l1 != null || l2 != null || carry >  0; ---> while loop continue. (Combine cases 1-7 or use demorgan's law of case 8)
+     *     2. l1 == null && l2 == null && carry == 0; ---> while loop break.
+     *
+     * Corner Cases:
+     *     1. l1 == null && l2 == null;   ---> doesn't need to handle, return value `dummy.next` is `null`
+     *     2. l1 and l2 only one is null; ---> doesn't need to handle.
+     *
+     * Time:  O(max(m, n)), exactly one pass, `m` and `n` is the length of l1 and l2, respectively.
      * Space: O(1)
      */
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        /**
-         * Don't need to handle if l1 == null or l2 == null
-         */
-        ListNode fakeHead = new ListNode(-1);
-        ListNode cur = fakeHead;
-        
+        ListNode dummy = new ListNode(-1);
+        ListNode cur = dummy;
         int carry = 0;
-        while (l1 != null && l2 != null) {
-            int sum = l1.val + l2.val + carry;
-            cur.next = new ListNode(sum % 10);
-            
-            l1 = l1.next;
-            l2 = l2.next;
-            cur = cur.next;
-            carry = sum / 10;
-        }
-        
-        ListNode temp = l1 == null ? l2 : l1;
-        
-        while (temp != null) {
-            int sum = temp.val + carry;
-            cur.next = new ListNode(sum % 10);
-            
-            temp = temp.next;
-            cur = cur.next;
-            carry = sum / 10;
-        }
-        
-        if (carry != 0) {
-            cur.next = new ListNode(carry);
-        }
-        
-        return fakeHead.next;
-    }
-
-    /**
-     * Optimization: while loop stop condition is l1 == null && l2 == null && carry == 0,
-     *               use De Morgan's law, while loop execute condition is
-     *               !(l1 == null && l2 == null && carry == 0)
-     *               which equals to l1 != null || l2 != null || carry != 0
-     */
-    /**
-     * Time: O(max(m, n))
-     * Space: O(1)
-     */
-    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode fakeHead = new ListNode(-1);
-        ListNode cur = fakeHead;
-        int carry = 0;
-
-        while (l1 != null || l2 != null || carry != 0) {
+        while (l1 != null || l2 != null || carry > 0) {
             int val1 = l1 == null ? 0 : l1.val;
             int val2 = l2 == null ? 0 : l2.val;
             int sum = val1 + val2 + carry;
-            cur.next = new ListNode(sum % 10);
-            
-            cur = cur.next;
             carry = sum / 10;
-            
+            cur.next = new ListNode(sum % 10);
+            cur = cur.next;
             l1 = l1 == null ? null : l1.next;
             l2 = l2 == null ? null : l2.next;
         }
-        
-        return fakeHead.next;
+        return dummy.next;
     }
 }
-
-
-
-
