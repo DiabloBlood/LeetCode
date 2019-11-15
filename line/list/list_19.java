a7f83a6095a8a5dc369d32dcaa0d217972fd01d9
DiabloBlood <huangsichao2015@gmail.com>
2019-11-14 22:23:31 -0800

    Added: comments and right code for 19

--- line/list/list_19.java
+++ line/list/list_19.java
@@ -4,24 +4,39 @@
 class Solution19 {
     /**
      * Problem Analysis:
-     *     1. Consider input 1->2->3->4->5->6->7->8->9->null, n = 3.
-     *     2. Use slow & fast pointers, when `fast` arrive at index `n` (NOTE: list is zero indexed),
-     *        which means node `4`, then `slow` start off, assign node `1` to `slow`.
-     *     3. When `fast` arrive `null`, `slow` will arrive index `length - n + 1`, which means node `7`.
-     *     4. Return `slow` as result;
+     *     1. This problem must use dummy node, otherwise if the node intend to remove is head,
+     *        we cannot get the previous node of head.
+     *     2. Dummy node index is `0`, tail node index is `len - 1`, `null` index is `len`,
+     *        when `fast` arrive index `n + 1`, then slow start off,
+     *        after `fast` arrive index `len`, which is `null`, `slow` will arrive index `len - n - 1`,
+     *        index of the code intended to remove is `len - n`. The relationship is as follow,
+     *
+     *                               fast        slow
+     *        slow start off:        n + 1       0
+     *        fast arrive `null`:    len         len - n - 1
+     *
+     * Corner Cases:
+     *     1. `n <= len` is guaranteed, which means `n` is always valid.
+     *     2. head == null; ---> this input is impossible, `slow.next.next` will throw `NullPointerException`.
+     *     3. head.next == null && n == 1; ---> finally `fast` at `null`, `slow` at index `0`, return value is `null`.
+     *
+     * Time:  O(n), exactly one pass, ptr `fast` go through this list exactly 1 time.
+     * Space: O(1)
      */
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        ListNode slow = head;
-        ListNode fast = head;
-        int count = 0;
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+        ListNode fast = dummy;
+        ListNode slow = dummy;
+        int index = 0;
         while (fast != null) {
-            if (count > n) {
+            if (index > n) {
                 slow = slow.next;
             }
             fast = fast.next;
-            count++;
+            index++;
         }
         slow.next = slow.next.next;
-        return slow;
+        return dummy.next;
     }
 }
