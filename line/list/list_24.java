ffe4c32b110533ab06e3351b8f15b1ebed0ce1dd
DiabloBlood <huangsichao2015@gmail.com>
2019-11-15 21:26:33 -0800

    Added: comments of list 24

--- line/list/list_24.java
+++ line/list/list_24.java
@@ -1,28 +1,38 @@
 
 
 
-class Solution {
-    /* 
+class Solution24 {
+    /**
+     * Problem Analysis:
+     *     1. Must use a dummy node for this problem, since `head` node will change positions.
+     *     2. Input: 1->2->3->4->5->6->null, output 2->1->4->3->6->5->null, finally `cur` at `5`.
+     *     3. Input: 1->2->3->4->5->null, output 2->1->4->3->5->null, finally `cur` at `3`.
+     *     4. We should guarantee at least have 2 nodes after prt `cur`.
+     *
      * Case Analysis:
-     * 1. n is odd,  finally cur is the last second node.
-     * 2. n is even, finally cur is tail.
-     * Time:  O(n)
+     *     1. cur.next == null || cur.next.next == null; ---> while loop break, depends on odd length or even length of this list.
+     *     2. cur.next != null && cur.next.next != null; ---> while loop continue. (Use demorgan's law)
+     *
+     * Corner Cases:
+     *     1. head == null;      ---> doesn't need to handle, return value `dummy.next` is `null`.
+     *     2. head.next == null; ---> doesn't need to handle, if list contains only one node, code will not run into while loop,
+     *                                return value `dummy.next` is `head`.
+     *
+     * Time:  O(n), exactly ont pass, every loop jump two nodes, total steps is `n / 2`
      * Space: O(1)
      */
     public ListNode swapPairs(ListNode head) {
-        ListNode fakeHead = new ListNode(-1);
-        fakeHead.next = head;
-        ListNode cur = fakeHead;
-        
+        ListNode dummy = new ListNode(-1);
+        ListNode cur = dummy;
+        dummy.next = head;
         while (cur.next != null && cur.next.next != null) {
-            ListNode first = cur.next;
-            ListNode second = cur.next.next;
-            first.next = second.next;
-            second.next = first;
-            cur.next = second;
-            cur = first;
+            ListNode node1 = cur.next;
+            ListNode node2 = cur.next.next;
+            node1.next = node2.next;
+            node2.next = node1;
+            cur.next = node2;
+            cur = node1; // cur = cur.next.next
         }
-        
-        return fakeHead.next;
+        return dummy.next;
     }
 }
