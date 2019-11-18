9ce0098c5b805fd83c52e0b33a927c78769256a4
DiabloBlood <huangsichao2015@gmail.com>
2019-11-18 09:02:04 +0000

    Added: comments of list 203

--- line/list/list_203.java
+++ line/list/list_203.java
@@ -2,10 +2,31 @@
 
 
 class Solution203 {
+    /**
+     * Problem Analysis:
+     *     1. Must use dummy node, since `head` may be removed.
+     *     2. `cur.next` as compare pointer, since nodes after `cur` may be removed.
+     *
+     * Case Analysis:
+     *     outer while loop:
+     *         1. cur.next == null; ---> while loop break;
+     *         2. cur.next != null; ---> while loop continue;
+     *     inner if else block:
+     *         1. cur.next.val == val; ---> cur.next = cur.next.next;
+     *            remove `cur.next`, but not move `cur` to next node, since next node may be removed.           
+     *         2. cur.next.val == val; ---> cur = cur.next;
+     * 
+     * Corner Cases:
+     *     1. head == null; ---> doesn't need to handle, return value `dummy.next` is `null`.
+     *     2. input `1->null`, `val == 1`; ---> return value `dummy.next` is `null`.
+     *
+     * Time:  O(n), exactly one pass.
+     * Space: O(1)
+     */
     public ListNode removeElements(ListNode head, int val) {
         ListNode dummy = new ListNode(-1);
         ListNode cur = dummy;
-        cur.next = head;
+        dummy.next = head;
         while (cur.next != null) {
             if (cur.next.val == val) {
                 cur.next = cur.next.next;
