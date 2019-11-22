b25f56cb1c9e535f553fca5c7938a0c4e32c4aa9
DiabloBlood <huangsichao2015@gmail.com>
2019-11-22 12:30:41 -0800

    Added: detail comments for tree 101

--- tree/tree_101.java
+++ tree/tree_101.java
@@ -3,16 +3,38 @@
 
 class Solution101 {
     /**
-     * Case Analysis:
-     * 1. p == null && q == null; return true; (Bottom Condition)
-     * 2. p == null && q != null; return false;
-     * 3. p != null && q == null; return false;
-     * 4. p != null && q != null && p.val != q.val; return false;
-     * 5. p != null && q != null && p.val == q.val && isSymmetric(left) && isSymmetric(right); return true;
+     * Method Tags:
+     *     1. Bottom Up.
+     *
+     * Problem Analysis:
+     *     1. If invert right subtree first (tree 226), the this problem been reduced to a same tree problem. (tree 100)
+     *     2. In fact, we don't need to invert a subtree, just recursively call isSame(p.left, q.right) and isSame(p.right, q.left).
+     *
+     * Base Cases:
+     *     1. p == null && q == null; ---> return true; // `null` has two kind of cases, from leaf node or from non-leaf node.
+     *     2. p == null && q != null; ---> return false;
+     *     3. p != null && q == null; ---> return false;
+     *     4. p != null && q != null && p.val != q.val; ---> return false;
+     *     combine 2, 3, 4:
+     *     2. p == null || q == null || p.val != q.val; ---> return false
+     *
+     * General Cases:
+     *     1. p != null && q != null && p.val == q.val && isSym(p.left, q.right) == false && isSym(p.right, q.left) == false;  ---> return false;
+     *     2. p != null && q != null && p.val == q.val && isSym(p.left, q.right) == false && isSym(p.right, q.left) ==  true;  ---> return false;
+     *     3. p != null && q != null && p.val == q.val && isSym(p.left, q.right) == true  && isSym(p.right, q.left) == false;  ---> return false;
+     *     4. p != null && q != null && p.val == q.val && isSym(p.left, q.right) == true  && isSym(p.right, q.left) ==  true;  ---> return true;
+     *     combine 1, 2, 3, 4:
+     *     1. return isSym(p.left, q.right) && isSym(p.right, q.left);
+     *        // if base cases not return, then `p != null && q != null && p.val == q.val` already guranteed.
+     *
+     * Corner Cases:
+     *     1. root == null; ---> return true; // must be handled, otherwise `root.left` and `root.right` will throw `NullPointerException`.
      * 
-     * Complexity: assume tree p has `m` nodes, tree q has `n` nodes.
-     * Time: best O(1), worst O(n) (In worst case m == n)
-     * Space: best O(1), avg O(logn) of complete binary tree, worst O(n) of flat list tree
+     * Time:  best  O(1), detected `p` and `q` is not symmetric at root level.
+     *        worst O(n/2), each subtree contains `n/2` nodes.
+     * Space: best  O(1), detected `p` and `q` is not symmetric at root level.
+     *        avg   O(logn), for height-balanced binary tree, complete binary tree, full binary tree.
+     *        worst O(n/2), for each subtree is a skewed binary tree. (Any shape)
      */
     public boolean isSymmetric(TreeNode root) {
         if (root == null) {
@@ -29,5 +51,61 @@ class Solution101 {
             return false;
         }
         return helper(p.left, q.right) && helper(p.right, q.left);
+    }
+
+    /**
+     * Method Tags:
+     *     1. Explicit Stack.
+     *
+     * Base Cases:
+     *     1. p == null && q == null; ---> continue; // `null` has two kind of cases, from leaf node or from non-leaf node.
+     *        // just `continue` if us explicit stack, to avoid push any node to stack equal to return from the bottom of recursion tree.
+     *     2. p == null && q != null; ---> return false;
+     *     3. p != null && q == null; ---> return false;
+     *     4. p != null && q != null && p.val != q.val; ---> return false;
+     *     combine 2, 3, 4:
+     *     2. p == null || q == null || p.val != q.val; ---> return false
+     *
+     * General Cases:
+     *     No general cases, since explicit stack could return in any stack level. (Implicit stack could only return at root level)
+     *     1. just push nodes to stack.
+     *
+     * Corner Cases:
+     *     1. root == null; ---> return true; // must be handled, otherwise `root.left` and `root.right` will throw `NullPointerException`.
+     *
+     * Time:  best  O(1), detected `p` and `q` is not symmetric at root level.
+     *        worst O(n/2), each subtree contains `n/2` nodes.
+     *        avg   O(n)
+     * Space: best  O(1), detected `p` and `q` is not symmetric at root level.
+     *        best  O(1), for each subtree is a skewed binary tree. (Any shape)
+     *        worst O(logn), for height-balanced binary tree, complete binary tree, full binary tree.
+     *        avg   O(logn)
+     */
+    private class Node {
+        TreeNode p;
+        TreeNode q;
+        Node (TreeNode node1, TreeNode node2) {
+            p = node1;
+            q = node2;
+        }
+    }
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        Deque<Node> stack = new ArrayDeque<>();
+        stack.push(new Node(root.left, root.right));
+        while (!stack.isEmpty()) {
+            Node cur = stack.pop();
+            if (cur.p == null && cur.q == null) {
+                continue;
+            }
+            if (cur.p == null || cur.q == null || cur.p.val != cur.q.val) {
+                return false;
+            }
+            stack.push(new Node(cur.p.right, cur.q.left));
+            stack.push(new Node(cur.p.left, cur.q.right));
+        }
+        return true;
     }
 }
