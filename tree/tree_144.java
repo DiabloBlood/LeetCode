96d646be5d4a17013df8254d9c894ae22912f200
DiabloBlood <huangsichao2015@gmail.com>
2019-12-02 12:06:39 -0800

    Added: detailed comments for tree 144

--- tree/tree_144.java
+++ tree/tree_144.java
@@ -13,7 +13,8 @@ class Solution144 {
      *
      * Time:  O(n), binary tree contains `n` nodes.
      * Space: best  O(logn), for height-balanced binary tree, complete binary tree, full binary tree.
-     *        worst O(n), for skewed binary tree.
+     *        worst O(n), for skewed binary tree. (Any shape of skewed binary tree, left skewed, right skewed, or zigzag shape)
+     *        avg   O(logn)
      */
     public List<Integer> preorderTraversal(TreeNode root) {
         List<Integer> result = new ArrayList<>();
@@ -30,13 +31,12 @@ class Solution144 {
         helper(result, node.right);
     }
 
-
     /**
      * Iteration Method
      *
      * Key Points:
      *     1. Push `cur.right` to stack first, since pre order is `root->left->right`,
-     *        push `right` child first will guarantee `left` child pop first. (Ref stack LIFO order)
+     *        push `right` child first will guarantee `left` child pop first. (Ref. stack LIFO order)
      *     2. `Deque` interface not allow `null` element.
      *     3. Why space for height-balanced binary tree, complete binary tree, full binary tree is O(logn)?
      *        Recursion method is hold every `root` nodes in implicit stack,
@@ -47,8 +47,9 @@ class Solution144 {
      *        // `Deque` interface not allow `null` element, otherwise will throw `NullPointerException` when call `stack.push` method.
      *
      * Time:  O(n), binary tree contains `n` nodes.
-     * Space: best O(1), for skewed binary tree. (Any kind of skewed binary tree, left skewed, right skewed, or zigzag shape)
+     * Space: best  O(1), for skewed binary tree. (Any shape of skewed binary tree, left skewed, right skewed, or zigzag shape)
      *        worst O(logn), for height-balanced binary tree, complete binary tree, full binary tree.
+     *        avg   O(logn)
      */
     public List<Integer> preorderTraversal(TreeNode root) {
         if (root == null) {
@@ -71,8 +72,21 @@ class Solution144 {
     }
 
     /**
-     * Time:  O(n)
-     * Space: best O(logn) of complete binary tree, worst O(n) of flat list tree
+     * General Cases:
+     *     1. stack.isEmpty()  && cur != null; ---> // there are to cases applied to this condition.
+     *         a. First time enter to while loop, at that time, `cur == root`.
+     *         b. After `root` and subtree `root.left` been traversed, at that time, `cur == root.right`.
+     *     2. !stack.isEmpty() && cur != null; ---> // Very trivial case, when `cur` is one of majority of internal nodes.
+     *     3. !stack.isEmpty() && cur == null; ---> // When `cur` is `right` of some nodes, and this `right` is `null`.
+     *     4. stack.isEmpty()  && cur == null; ---> // while loop break, all nodes been traversed.
+     *
+     * Corner Cases:
+     *     1. root == null; ---> // doesn't need to handle, return value `result` is an empty array list.
+     *
+     * Time:  O(n), binary tree contains `n` nodes.
+     * Space: best  O(1), for right skewed binary tree.
+     *        worst O(n), for left skewed binary tree. (L->R->L->R->L->R zigzag skewed tree is O(n/2), `n/2` nodes poped and `n/2` nodes inside stack)
+     *        avg   O(logn), for height-balanced binary tree, complete binary tree, full binary tree.
      */
     public List<Integer> preorderTraversal(TreeNode root) {
         List<Integer> result = new ArrayList<>();
