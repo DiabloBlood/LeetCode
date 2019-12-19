08410e3d649541763a7f2b658063aa2fe750307f
DiabloBlood <huangsichao2015@gmail.com>
2019-12-18 20:52:26 -0800

    Modified: some comments of tree 226

--- tree/rb/rb_226.java
+++ tree/rb/rb_226.java
@@ -7,18 +7,19 @@ class Solution226 {
      *     1. Bottom Up.
      *
      * Problem Analysis:
+     *     1. Invert tree could seen as mirror symmetric.
      *     1. Swap `node.left` and `node.right` after left subtree and right subtree already been inverted.
      *     2. Inversion takes place at leaf nodes first, then takes place at upper/root nodes.
      *
      * Base Cases;
-     *     1. root == null; ---> return null; // `null` has two kind of cases, from leaf node or from non-leaf node.
+     *     1. root == null; ---> return null; // `null` has two kind of cases, from a leaf node or from one-child parent node.
      *
      * Corner Cases:
-     *     1. input root == null; ---> // doesn't need to handle, already been handled by base cases.
+     *     1. input root == null; ---> // doesn't need to handle, already handled by base cases.
      *
      * Time:  O(n), binary tree contains `n` nodes.
      * Space: best  O(logn), for height-balanced binary tree, complete binary tree, full binary tree.
-     *        worst O(n), for skewed binary tree. (Any shape)
+     *        worst O(n), for skewed binary tree (Any shape).
      *        avg   O(logn)
      */
     public TreeNode invertTree(TreeNode root) {
@@ -35,19 +36,15 @@ class Solution226 {
      * Method Tags:
      *     1. Top down recursion
      *
-     * Problem Analysis:
-     *     1. Swap `node.left` and `node.right` before left subtree and right subtree already been inverted.
-     *     2. Inversion takes place at upper/root nodes first, then takes place at leaf nodes.
-     *
      * Base Cases;
-     *     1. node == null; ---> return; // `null` has two kind of cases, from leaf node or from non-leaf node.
+     *     1. node == null; ---> return; // `null` has two kind of cases, from a leaf node or from one-child parent node.
      *
      * Corner Cases:
-     *     1. input root == null; ---> // doesn't need to handle, already been handled by base cases.
+     *     1. input root == null; ---> // doesn't need to handle, already handled by base cases.
      *
      * Time:  O(n), binary tree contains `n` nodes.
      * Space: best  O(logn), for height-balanced binary tree, complete binary tree, full binary tree.
-     *        worst O(n), for skewed binary tree. (Any shape)
+     *        worst O(n), for skewed binary tree (Any shape)
      *        avg   O(logn)
      */
     public TreeNode invertTree(TreeNode root) {
