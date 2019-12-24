fd99f95804607229c96d0502906f57853c0c1c82
DiabloBlood <huangsichao2015@gmail.com>
2019-12-23 19:51:14 -0800

    Added: tree 230

---
+++ tree/tree_230.java
@@ -0,0 +1,22 @@
+
+
+
+class Solution230 {
+    public int kthSmallest(TreeNode root, int k) {
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        TreeNode cur = root;
+        while (!stack.isEmpty() || cur != null) {
+            while (cur != null) {
+                stack.push(cur);
+                cur = cur.left;
+            }
+            cur = stack.pop();
+            k--;
+            if (k == 0) {
+                return cur.val;
+            }
+            cur = cur.right;
+        }
+        return -1;
+    }
+}
