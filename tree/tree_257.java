43a79e7bc00165adba562e0ed332f1bb39aacf0f
DiabloBlood <huangsichao2015@gmail.com>
2019-12-30 09:36:45 +0000

    Added: some comments of tree 257

--- tree/tree_257.java
+++ tree/tree_257.java
@@ -1,58 +1,89 @@
 
 
 
-class Solution107 {
+class Solution257 {
 	/**
      * DFS Method
      *
      * Key Points:
-     *    1. Add `node.val` to path list when enter a recursive call, remove it when return this recursive call.
+     *    1. Add `node.val` and `prefix` to path stringbuilder when enter a recursive call, remove it when return this recursive call.
+     *    1. Use `setLength` method of stringbuilder to remove characters.
      *
      * Base Cases:
-     *     1. node == null; ---> return; // `null` from a one-child parent node, not from leaf node. Or `root` is `null`.
+     *     1. node == null; ---> return; // case 1: `null` from a one-child parent.
+     *                                   // case 2: `null` from a leaf node.
      *
      * General Cases:
      *     1. node.left == null && node.right == null; ---> arrive to leaf node, add path string.
      *     2. node.left != null || node.right != null; ---> // internal node, do nothing
      *
      * Corner Cases:
-     *     1. root == null; ---> doesn't need to handle, return value `result` is a empty array list.
+     *     1. root == null; ---> // doesn't need to handle, already handled by base cases.
      *
-     * Time:  best  O(2n), for skewed binary tree (Any shape), skewed tree only has one leaf node,
-     *                     the largest path list size is `n`, DFS takes O(n) time, generate path string takes O(n) time.
+     * Time:  best  O(2n), for skewed binary tree (Any shape), skewed tree only has one leaf node, the largest path size is `n`,
+     *                     DFS takes O(n) time, generate path string takes O(n) time (`path.toString()` method  takes O(n)).
      *        worst O(n + (n/2)*logn), for full binary tree and complete binary tree, in this case tree has `n/2` leaf nodes,
-     *                                  the largest path list size is `logn`, DFS takes O(n) time, generate path string takes O((n/2)*logn) time.
+     *                                 the largest path size is `logn`, DFS takes O(n) time, generate path string takes O((n/2)*logn) time.
      *        avg   O(n + (n/c)*logn), `c` is larger than `2`.
-     * Space: best  O(2logn), for height-balanced binary tree, complete binary tree, full binary tree.
-     *                         Path list takes O(logn) space, implicit stack takes O(logn) space.
-     *        worst O(2n), for skewed binary tree (Any shape), Path list takes O(n) space, implicit stack takes O(n) space.
+     * Space: best  O(2logn), for height-balanced binary tree, complete binary tree, full binary tree,
+     *                        path takes O(logn) space, implicit stack takes O(logn) space.
+     *        worst O(2n), for skewed binary tree (Any shape), path takes O(n) space, implicit stack takes O(n) space.
      *        avg   O(2logn), since majority input trees are height-balanced binary trees.
      *
-     * Notes: result size is O(n) ~ O((n/2)*logn)
+     * Notes: result size is O(n) ~ O((n/2)*logn), only count `node.val`, not count `->`.
      */
     public List<String> binaryTreePaths(TreeNode root) {
         List<String> result = new ArrayList<>();
-        List<Integer> path = new ArrayList<>();
-        helper(result, path, root);
+        helper(result, new StringBuilder(), "", root);
         return result;
     }
-
-    private void helper(List<String> result, List<Integer> path, TreeNode node) {
+    
+    private void helper(List<String> result, StringBuilder path, String prefix, TreeNode node) {
         if (node == null) {
             return;
         }
-        path.add(node.val);
+        int len = path.length();
+        path.append(prefix).append(node.val);
         if (node.left == null && node.right == null) {
-            StringBuilder sb = new StringBuilder();
-            String prefix = "";
-            for (int val: path) {
-                sb.append(prefix).append(val);
-                prefix = "->";
-            }
-            result.add(sb.toString());
+            result.add(path.toString());
         }
-        helper(result, path, node.left);
-        helper(result, path, node.right);
-        path.remove(path.size() - 1);
+        helper(result, path, "->", node.left);
+        helper(result, path, "->", node.right);
+        path.setLength(len);
+    }
+
+    // notes: constructor `StringBuilder(int capacity)`
+    public List<String> binaryTreePaths(TreeNode root) {
+        if (root == null) {
+            return new ArrayList<>();
+        }
+        List<String> result = new ArrayList<>();
+        Queue<TreeNode> queue = new ArrayDeque<>();
+        Queue<StringBuilder> paths = new ArrayDeque<>();
+        queue.offer(root);
+        paths.offer(new StringBuilder(Integer.toString(root.val)));
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode cur = queue.poll();
+                StringBuilder curPath = paths.poll();
+                if (cur.left == null && cur.right == null) {
+                    result.add(curPath.toString());
+                }
+                if (cur.left != null) {
+                    queue.offer(cur.left);
+                    StringBuilder sb = new StringBuilder(curPath);
+                    sb.append("->").append(cur.left.val);
+                    paths.offer(sb);
+                }
+                if (cur.right != null) {
+                    queue.offer(cur.right);
+                    StringBuilder sb = new StringBuilder(curPath);
+                    sb.append("->").append(cur.right.val);
+                    paths.offer(sb);
+                }
+            }
+        }
+        return result;
     }
 }
