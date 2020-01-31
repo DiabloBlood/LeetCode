99e7375946206d5f0f414f06a0d011d74e868bae
DiabloBlood <huangsichao2015@gmail.com>
2020-01-30 20:54:54 -0800

    Added: detailed comments for tree 114

--- tree/tree_list/tl_114.java
+++ tree/tree_list/tl_114.java
@@ -3,51 +3,76 @@
 
 class Solution114 {
     /**
-     * Analysis: input [1,2,5,3,4,null,6]
-     *      1
-     *     / \
-     *    2   5
-     *   / \   \
-     *  3   4   6
-     * 
-     * 1. Output `1->2->3->4->5->6` is pre-order, `Root-->L-->R`.
-     * 2. The output should build as the order of `R-->L-->Root`, which is very like post-order traversal.
-     * 3. dummy->null, dummy->6->null, dummy->5->6->null, finally dummy->1->2->3->4->5->6->null.
-     * 
-     * Time:  O(n), the only way to get result is to traverse all the nodes.
-     * Space: Best  O(logn) for balanced tree.
-     *        Worst O(n) for flat list tree.
+     * Pre-order traversal method
+     *
+     * Problem Analysis:
+     *     1. If doesn't have code `node.left = null`, final result will not be a right skewed tree, please see below
+     *            1               1                 1
+     *           / \             / \              /   \
+     *          2   5    --->    `->2    --->    2     2
+     *         / \   \             / \          / \   / \
+     *        3   4   6            `->3        3   3 3   3
+     *                                 \        \   \ \   \
+     *                                  4        4   4 4   4
+     *                                   \        \   \ \   \
+     *                                    5        5   5 5   5
+     *                                     \        \   \ \   \
+     *                                      6        6   6 6   6
+     *     2. Build tree from `R-->L-->Root`, very likes post-order, dummy->null, dummy->6->null,
+     *        dummy->5->6->null,, ..., dummy->1->2->3->4->5->6->null, which means insert at head.
+     *     3. `dummy` will not lead to memory leak, since `root` has other reference and `dummy` will not has any reference
+     *        after method `flatten` return, finally `dummy` will be collected by GC after return.
+     *
+     * Base Cases:
+     *     1. node == null; ---> return; // just return
+     *
+     * Corner Cases:
+     *     2. root == null; ---> // doesn't need to handle, since main method `flatten` is a void method.
+     *                              If return list head is required, return `dummy.right` is always right.
+     *
+     * Time:  O(n), binary tree contains `n` nodes.
+     * Space: best  O(logn), for height-balanced binary tree, complete binary tree, full binary tree.
+     *        worst O(n), for skewed binary tree (Any Shape).
+     *        avg   O(logn)
      */
     public void flatten(TreeNode root) {
         TreeNode dummy = new TreeNode(-1);
         helper(dummy, root);
-        //node dummy.right is root
     }
-    
+
     private void helper(TreeNode dummy, TreeNode node) {
         if (node == null) {
             return;
         }
         helper(dummy, node.right);
         helper(dummy, node.left);
-        node.right = dummy.right;
         node.left = null;
-        dummy.right = node;  
+        node.right = dummy.right;
+        dummy.right = node;
     }
 
     /**
      * Iteration Method (Pre-order, root->L->R)
      *
-     * Problem Pitfalls:
-     *     1. `head.left = null` is very important.
-     *
      * Problem Analysis:
-     *     1. Build result from pre-order. `dummy->null`, `dummy->1->null`, `dummy->1->2->null`.
-     *     2. `head` start from `dummy`, and always point to `tail` node.
+     *     1. If doesn't have code `node.left = null`, the final result will not be a right skewed tree, please see below
+     *            1               1                 1
+     *           / \             / \              /   \
+     *          2   5    --->    `->2    --->    2     2
+     *         / \   \             / \          / \   / \
+     *        3   4   6            `->3        3   3 3   3
+     *                                 \        \   \ \   \
+     *                                  4        4   4 4   4
+     *                                   \        \   \ \   \
+     *                                    5        5   5 5   5
+     *                                     \        \   \ \   \
+     *                                      6        6   6 6   6
+     *     2. Build tree from pre-order, dummy->null, dummy->1->null, dummy->1->2>null, ..., dummy->1->2->3->4->5->6->null.
+     *     3. `cur` is always the tail node of the linked list.
      *
      * Corner Cases:
-     *     1. root == null; ---> return;
-     *        // `Deque` interface not allow `null` element, otherwise will throw `NullPointerException` when call `stack.push` method.
+     *     1. root == null; ---> return; // `Deque` interface not allow `null` element, otherwise will
+     *                                      throw `NullPointerException` when call `stack.push` method.
      *
      * Time:  O(n), binary tree contains `n` nodes.
      * Space: best  O(1), for skewed binary tree (Any shape). 
@@ -76,30 +101,54 @@ class Solution114 {
     }
 
     /**
-     * Analysis:
-     * 1. Build result in pre-order. dummy->null, dummy->1->null, dummy->1->2->null.
-     * 
-     * Time:  O(n), the only way to get result is to traverse all the nodes.
-     * Space: Best  O(logn) for balanced tree.
-     *        Worst O(n) for flat list tree.
+     * Pre-order recursion traversal method
+     *
+     * Problem Analysis:
+     *     1. If doesn't have code `node.left = null`, the final result will not be a right skewed tree, please see below
+     *            1               1                 1
+     *           / \             / \              /   \
+     *          2   5    --->    `->2    --->    2     2
+     *         / \   \             / \          / \   / \
+     *        3   4   6            `->3        3   3 3   3
+     *                                 \        \   \ \   \
+     *                                  4        4   4 4   4
+     *                                   \        \   \ \   \
+     *                                    5        5   5 5   5
+     *                                     \        \   \ \   \
+     *                                      6        6   6 6   6
+     *     2. Build tree from pre-order, dummy->null, dummy->1->null, dummy->1->2>null, ..., dummy->1->2->3->4->5->6->null.
+     *     3. `cur` is always the tail node of the linked list.
+     *     4. `dummy` will not lead to memory leak, since `root` has other reference and `dummy` will not has any reference
+     *        after method `flatten` return, finally `dummy` will be collected by GC after return.
+     *
+     * Base Cases:
+     *     1. node == null; ---> return cur; // which means `cur` cannot move to next node.
+     *
+     * Corner Cases:
+     *     2. root == null; ---> // doesn't need to handle, since main method `flatten` is a void method.
+     *                              If return list head is required, return `dummy.right` is always right.
+     *
+     * Time:  O(n), binary tree contains `n` nodes.
+     * Space: best  O(logn), for height-balanced binary tree, complete binary tree, full binary tree.
+     *        worst O(n), for skewed binary tree (Any Shape).
+     *        avg   O(logn)
      */
     public void flatten(TreeNode root) {
-        TreeNode tail = new TreeNode(-1);
-        helper(tail, root);
+        TreeNode dummy = new TreeNode(-1);
+        helper(dummy, root);
     }
     
-    private TreeNode helper(TreeNode tail, TreeNode node) {
+    private TreeNode helper(TreeNode cur, TreeNode node) {
         if (node == null) {
-            return tail;
+            return cur;
         }
         TreeNode left = node.left;
         TreeNode right = node.right;
-        
-        tail.right = node;
-        tail.left = null;
-        tail = node;    //tail = tail.right
-        tail = helper(tail, left);
-        tail = helper(tail, right);
-        return tail;
+        node.left = null;
+        cur.right = node;
+        cur = cur.right;
+        cur = helper(cur, left); // now cur.right is no longer to be node.right, which is the tail of left subtree.
+        cur = helper(cur, right);
+        return cur;
     }
 }
