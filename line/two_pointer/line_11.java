cd307253a2bffdcd366fb8b447033598e89f587b
DiabloBlood <huangsichao2015@gmail.com>
2020-03-04 10:17:42 +0000

    Added: detailed comments for line two pointer 11

--- line/two_pointer/line_11.java
+++ line/two_pointer/line_11.java
@@ -4,20 +4,22 @@
 public class Solution11 {
     /**
      * Problem Analysis:
-     *     1. 
+     *     1. The key point is for any point `i`, we doesn't need to brute force check all points in [i + 1, n - 1] one by one,
+     *        when the first `j` found that h[i] < h[j], all points in [i + 1, j - 1] doesn't need to check.
      *
      * General Cases:
      *     1. h[i] <  h[j]; ---> record (j - i) * h[i]; i++; // i with any line in [i + 1, j - 1] is impossible larger than `(j - i) * h[i]`.
      *     2. h[i] >= h[j]; ---> record (j - i) * h[j]; j--; // j with any line in [i + 1, j - 1] is impossible larger than `(j - i) * h[j]`.
      *
      * Corner Cases:
-     *     1. h == null; ---> return 0;
+     *     1. h == null; ---> return 0; // otherwise `int j = h.length - 1;` will throw `NullPointerException`.
+     *     2. h.length < 2; ---> // non valid input
      *
-     * Time:
-     * Space:
+     * Time:  O(n)
+     * Space: O(1)
      */
     public int maxArea(int[] h) {
-        if (h == null) {
+        if (h == null || h.length < 2) {
             return 0;
         }
         int result = Integer.MIN_VALUE;
@@ -29,6 +31,7 @@ public class Solution11 {
         }
         return result;
     }
+
     /**
      * Brute force method.
      *
@@ -36,7 +39,7 @@ public class Solution11 {
      * Space: O(1)
      */
     public int maxArea(int[] h) {
-        if (h == null) {
+        if (h == null || h.length < 2) {
             return 0;
         }
         int result = Integer.MIN_VALUE;
