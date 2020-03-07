b50a3033ef68aaee3ef4125f742c633613f83e06
DiabloBlood <huangsichao2015@gmail.com>
2020-03-06 19:54:32 -0800

    Added: detailed comments for line one pass 12

--- line/one_pass/line_12.java
+++ line/one_pass/line_12.java
@@ -3,26 +3,28 @@
 
 public class Solution12 {
     /**
-     * Problem Pitfalls:
+     * Notes:
      *     1. Input number between [1, 3999]. This is the limitation of roman number.
      *
      * Problem Analysis:
-     *     1. Build map rules for roman number and arab number.
-     *     2. Calculate quotient of `num / arab[i]`;
-     *     3. The count of quotient is the count of roman characters that should be appended.
-     *     4. Assign remainder of `num / arab[i]` after every single loop.
-     *   
+     *     1. Use one string array and one integer array build array map, which maps roman digits and arabic digits.
+     *        The reason that cannot use one array map is because roman two char digits likes "CM", "CD"...
+     *     2. `count` between [0, 3].
+     *
+     * Corner Cases:
+     *     1. num <= 0 || num > 3999; ---> throw new IllegalArgumentException(); // invalid input
+     *
      * Time:  O(1), first for loop has `13` times operations, second for loop at most is `3` times operations.
-     * Space: O(1), string builder length is not unlimited!
+     * Space: O(1), string builder characters only takes O(1) space.
      */
     public String intToRoman(int num) {
-        if (num < 1 || num > 3999) {
-            return "";
+        if (num <= 0 || num > 3999) {
+            throw new IllegalArgumentException();
         }
-        String[] roman = new int[] {"I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"};
-        int[] arab = new int[] {1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000};
+        String[] roman = new String[] {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        int[] arab = new int[] {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
         StringBuilder sb = new StringBuilder();
-        for (int i = roman.length - 1; i >= 0; i--) {
+        for (int i = 0; i < roman.length; i++) {
             int count = num / arab[i];
             num %= arab[i];
             for (int j = 0; j < count; j++) {
