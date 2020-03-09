74fe1af9691afb47c1d022d02401f44aba374df1
DiabloBlood <huangsichao2015@gmail.com>
2020-03-09 12:16:57 -0700

    Added: detailed comments of line one pass 1

--- line/one_pass/line_1.java
+++ line/one_pass/line_1.java
@@ -1,43 +1,43 @@
 
 
 
-/*
-Given an array of integers, return indices of the two numbers such that they add up to a specific target.
-
-You may assume that each input would have exactly one solution, and you may not use the same element twice.
-
-Example:
-
-Given nums = [2, 7, 11, 15], target = 9,
-
-Because nums[0] + nums[1] = 2 + 7 = 9,
-return [0, 1].
-*/
-import java.util.HashMap;
-import java.util.Map
-
 public class Solution1 {
-
-    // O(n)
+    /**
+     * Notes:
+     *     1. Assume that each input would have exactly one solution.
+     *     2. You may not use the same element twice.
+     *
+     * Problem Analysis:
+     *     1. Use a hashmap, array value as map key, array index as map value.
+     *
+     * General Cases:
+     *     1. map.containsKey(target - nums[i]);  ---> return new int[] { map.get(target - nums[i]), i };
+     *     2. !map.containsKey(target - nums[i]); ---> map.put(nums[i], i);
+     *
+     * Corner Cases:
+     *     1. nums == null; ---> return new int[] { -1, -1 }; // otherwise `i < nums.length;` will throw `NullPointerException`.
+     *     2. nums.length == 0; ---> // doesn't need to handle, will skip for loop and return { -1, -1 }.
+     *
+     * Time:  O(n), for loop takes `n` time.
+     * Space: O(n), hashmap at most takes `n` space.
+     */
     public int[] twoSum(int[] nums, int target) {
-        int[] result = new int[] {-1, -1};
         if (nums == null) {
-            return result;
+            return new int[] { -1, -1 };
         }
         Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
-            int key = target  - nums[i];
-            if (map.containsKey(key)) {
-                result[0] = map.get(key);
-                result[1] = i;
-                return result;
+            if (map.containsKey(target - nums[i])) {
+                return new int[] { map.get(target - nums[i]), i };
             }
             map.put(nums[i], i);
         }
-        return result;
+        return new int[] { -1, -1 };
     }
 
-    // O (n^2) algorithm 
+    /**
+     * O(n^2) algorithm
+     */
     public int[] twoSum2(int[] nums, int target) {
         int[] result = new int[] {-1, -1};
         if (nums == null) {
