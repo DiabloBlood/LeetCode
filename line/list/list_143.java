ab4237fe63714f76865c683bd74cf92aa29b6a41
DiabloBlood <huangsichao2015@gmail.com>
2020-12-19 18:25:23 -0800

    Improvement of 143

--- line/list/list_143.java
+++ line/list/list_143.java
@@ -2,17 +2,29 @@
 
 
 class Solution143 {
-
     /* 
      * Problem Analysis:
      *     1. Find the middle node first.
      *     2. Then reverse the second list.
      *     3. Merge l1 and l2.
      *
-     * Time:  O(n), findMid O(n/2), reverse (n/2), merge(n /2)
+     * Notes: why `mid.next = null`?
+     *     1. Input 1->2->3->4->null, merge l1: 1->2->3->null and l2: 4->3->null
+     *         - l1: 1->4->2->3->null, l2: 3->null
+     *         - l1: 1->4->2->3->3, l2: null
+     *         - Finally, `3` directs to itself, list has cycle
+     *     2. Input 1->2->3->4->5->null, merge l1: 1->2->3->4->null and l2: 5->4->null
+     *         - l1: 1->5->2->3->4->null, l2: 4->null
+     *         - l1: 1->5->2->4->3->4->4, l2: null
+     *         - Finally, `3` directs to itself, list has cycle
+     *
+     * Corner Cases:
+     *     1. head == null; ---> return; // must handle, otherwise `findMid` method will throw 'NullPointerExcepition'.
+     *
+     * Time:  O(n), findMid O(n/2), reverse (n/2), merge (n/2)
      * Space: O(1)
      */
-public void reorderList(ListNode head) {
+    public void reorderList(ListNode head) {
         if (head == null) {
             return;
         }
@@ -23,7 +35,9 @@ public void reorderList(ListNode head) {
     }
 
     /**
-     * Samples of merge method:
+     * Merge Method
+     *
+     * Problem Analysis:
      *     1. Input 1->2->3->4->null, merge l1: 1->2->null and l2: 4->3->null
      *         - l1: 1->4->2->null, l2: 3->null
      *         - l1: 1->4->2->3->null, l2: null
@@ -33,16 +47,16 @@ public void reorderList(ListNode head) {
      *         - l1: 1->5->2->4->3->null, l2: null
      *         - Finally, l1 at `3`, l2 at `null`
      *
-     * Case Analysis of merge method:
-     *     1. l2 != null, merge
-     *     2. l2 == null, merge finished, stop while loop.
+     * General Cases:
+     *     1. l2 != null; ---> merge
+     *     2. l2 == null; ---> merge finished, while loop break
      */
     private void merge(ListNode l1, ListNode l2) {
         while (l2 != null) {
             ListNode temp = l2.next;
             l2.next = l1.next;
             l1.next = l2;
-            l1 = l2.next;
+            l1 = l2.next;    // or l1.next.next;
             l2 = temp;
         }
     }
