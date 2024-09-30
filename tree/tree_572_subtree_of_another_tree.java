68f6b3b5595e8da19bde2b84586ebb433dcf3bcc
DiabloBlood <huangsichao2015@gmail.com>
2024-09-29 23:31:09 -0700

    Some recent changes

--- tree/tree_572_subtree_of_another_tree.java
+++ tree/tree_572_subtree_of_another_tree.java
@@ -2,6 +2,24 @@
 
 
 class Solution572 {
+    /**
+     * Key Points:
+     *     1. For every node of `root`, verify if `subRoot` is a same tree of `root`.
+     *     2. If `subRoot` is a not same tree of `root`, recursively verify if `subRoot` is a subtree of `root.left` and `root.right`.
+     *
+     * Base Cases:
+     *     1. root == null; ---> return subRoot == null; // `null` is `null`'s subtree, `null` is subtree of any node
+     *
+     * Corner Cases:
+     *     1. root == null; ---> doesn't need to handle, return value is `subRoot == null`.
+     *
+     * Time:  best O(nlogn), binary tree contains `n` nodes, n + 2 * n/2 + 4 * n/4 + ... + n = nlogn,
+     *            - for height-balanced binary tree, complete binary tree, full binary tree.
+     *        worst O(n^2), for skewed binary tree. (Any shape)
+     * Space: best  O(logn), for height-balanced binary tree, complete binary tree, full binary tree.
+     *        worst O(n), for skewed binary tree. (Any shape)
+     *        avg   O(logn)
+     */
     public boolean isSubtree(TreeNode root, TreeNode subRoot) {
         if (root == null) {
             return subRoot == null;
